346de6c127acbc6227baebd5505ab4fbe001ad8a
John Souza <souzajohnlee@gmail.com>
2020-02-03 16:04:09 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,7 +4,8 @@ require 'directors_database'
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-
+  gross = 0
+  return gross
 end
 
 # Write a method that, given an NDS creates a new Hash
@@ -15,3 +16,5 @@ def directors_totals(nds)
   result = {}
   nil
 end
+
+pp directors_totals
